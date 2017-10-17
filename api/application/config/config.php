aba1aea1d5e21a58e9441169f07c475b386a3aff
wyj <ismrwang@gmail.com>
2017-10-17 10:19:44 +0800

    修复iview 版本过高的问题

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -521,3 +521,7 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+// 时区设置
+date_default_timezone_set('Asia/ShangHai');
+$config['time_zone'] = date_default_timezone_get();
