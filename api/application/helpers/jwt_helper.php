5b7cc7525d83c75932ef196d27d61279e0aa5b6f
wyj <ismrwang@gmail.com>
2017-12-20 12:58:40 +0800

    修复Token过期验证

--- api/application/helpers/jwt_helper.php
+++ api/application/helpers/jwt_helper.php
@@ -4,7 +4,7 @@ class jwt_helper extends CI_Controller
 {
     const CONSUMER_KEY = 'questionnaire';
     const CONSUMER_SECRET = 'questionnaire';
-    const CONSUMER_TTL = 1440;
+    const CONSUMER_TTL = 14400;
 
     // 生成 token
     public static function create($userid)
@@ -26,8 +26,15 @@ class jwt_helper extends CI_Controller
         $CI =& get_instance();
         $CI->load->library('JWT');
         try {
-            $CI->jwt->decode($token, self::CONSUMER_SECRET);
-            return true;
+            $decodeToken = $CI->jwt->decode($token, self::CONSUMER_SECRET);
+            // token 有效期验证
+            $ttl_time = strtotime($decodeToken->issuedAt);
+            $now_time = strtotime(date(DATE_ISO8601, strtotime("now")));
+            if(($now_time - $ttl_time) > $decodeToken->ttl) {
+                throw new Exception('Expired');
+            } else {
+                return true;
+            }
         } catch (Exception $e) {
             return false;
         }
